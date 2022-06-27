3d2b292f7fe37adaf3b10ccf6fb2a9b2eb2329bb
mohammad-al-amin-islam <alamin.islambcpsc@gmail.com>
2022-06-27 16:52:09 +0600

    navbar added

---
+++ src/Pages/Shared/Navabar/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import img from '../../../Images/icon.PNG'
+const Navbar = () => {
+    return (
+        <div className='container'>
+            <img src={img} alt="" />
+        </div>
+    );
+};
+
+export default Navbar;
